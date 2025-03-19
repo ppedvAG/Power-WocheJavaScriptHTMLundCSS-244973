219c0c8ddacf28b125fc5e9a3c78d1e8926dda92
DanielR <113525395+Shimotakii@users.noreply.github.com>
2025-03-19 15:33:03 +0100

    Add files via upload

---
+++ uebung/uebung.js
@@ -0,0 +1,49 @@
+/*
+
+    HTML:
+        Header:
+            Eine Überschrift "Mission: Reisen"
+
+        Main Bereich:
+            Mission: Ein kurzer Text, der die Mission und das Raumschiff beschreibt
+            Planeten-Stationen: Eine ungeordnete Liste, in der mindestens fünf Planeten oder Stationen
+            aufgeführt sind, die du auf deiner Reise besuchen möchtest.
+            Reisedaten: Ein Abschnitt, in dem später mithilfe von JavaScript einfache Berechnungen (z.B. 
+            die Gesamtdistanz oder durchschnittliche Reisezeit) präsentiert wird
+        
+        Footer:
+            Ein kleiner Hinweis oder Copyright-Informationen (z.B. "© 2025 Intergalaktische Reise")
+
+    CSS:
+        Layout: 
+            Zentriere den inhalt (z.B. mit Flexbox oder Grid)
+            Verwende einen dunklen Hintergrund, der an den Weltraum erinnert (z.b. tiefes Blau, grau, etc...)
+        
+        Farben:
+            Überschriften in leuchtendem Blau oder einem kontrastreichen Farbton
+            Text in einer gut lesbaren Farbe (z.B. Weiß oder Hellgrau)
+        
+        Besondere Effekte:
+            Runde Ecken und einen dezente Schatten für Abschnitte
+            Optional: Eine CSS-Animation
+
+    JS:
+        Variable deklarieren:
+            Erstelle eine Konstante spaceship mit dem Namen deines Raumschiffs und gib diesen in der Konsole aus
+
+        Array & Schleife:
+            Erstelle ein Array distanzen mit fünf Zahlen, die die Entfernungen (in Lichtjahren oder anderen
+            Einheiten) zu den Planeten repräsentieren
+            Verwende eine Schleife, um die Gesamtdistanz zu berechnen
+            Gib die Gesamtdistanz in der Konsole aus
+
+        If-Abfrage:
+            Schreibe eine if-Bedingung: Falls die Gesamtdistanz größer als ein bestimmter Wert (100) ist,
+            gib in der Konsole "Lange Reise" aus, sonst "Kurze Reise"
+        
+        Lambda-Funktion:
+            Definiere eine Lambda calcTravelTime, die basierend auf einer gegebenen Geschwindigkeit
+            (z.B. 20 Einheiten pro Zeit) die Reisezeit berechnet.
+            Teste diese Funktion mit der Gesamtdistanz und gib das Ergebnis in der Konsole aus.
+
+ */
